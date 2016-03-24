a10c438ae13fb987709d23828ce3e764d5b5c6ab
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-24 16:25:14 +0900

    First commit

---
+++ Oreo/Cell.swift
@@ -0,0 +1,17 @@
+//
+//  Cell.swift
+//  Oreo
+//
+//  Created by 荒川陸 on 2016/03/24.
+//  Copyright © 2016年 Mikan Laboratories. All rights reserved.
+//
+
+import Foundation
+
+enum Cell {
+    case Ship
+    case Blank
+    case attacked_Ship
+    case attacked_Blank
+    
+}
---
+++ Oreo/Field.swift
@@ -0,0 +1,23 @@
+//
+//  Field.swift
+//  Oreo
+//
+//  Created by 荒川陸 on 2016/03/24.
+//  Copyright © 2016年 Mikan Laboratories. All rights reserved.
+//
+
+import Foundation
+
+class Field {
+    let fieldSize = 10
+    var cell_arr : [[Int]]
+    
+    init(){
+        for var i=0;i<self.fieldSize; i++ {
+            
+        }
+    }
+    
+    
+    
+}
