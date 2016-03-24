e3a05e8afd7d64c00ef9879759a4c2234536466e
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-24 16:12:44 +0900

    Initial Commit

---
+++ Oreo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oreo
+//
+//  Created by 荒川陸 on 2016/03/24.
+//  Copyright © 2016年 Mikan Laboratories. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
