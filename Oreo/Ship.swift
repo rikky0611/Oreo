942db298d97aa4d80e052c1e2af1525f4359a7ff
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-24 20:20:38 +0900

    Create Ship class

---
+++ Oreo/Ship.swift
@@ -0,0 +1,18 @@
+//
+//  Ship.swift
+//  Oreo
+//
+//  Created by 荒川陸 on 2016/03/24.
+//  Copyright © 2016年 Mikan Laboratories. All rights reserved.
+//
+
+import Foundation
+
+class Ship {
+    enum Type{
+        case Carrier    //空母 5マス
+        case Cruiser    //巡洋艦 4マス
+        case Submarine  //潜水艦 3マス
+        case Destroyer  //駆逐艦 2マス
+    }
+}
